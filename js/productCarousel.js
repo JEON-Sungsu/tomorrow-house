397b9cc9f621fa134bf3af1734189d405228ab29
JEON-Sungsu <63297236+JEON-Sungsu@users.noreply.github.com>
2022-01-19 19:01:10 +0900

    tiny-slider import 및 product-carousel making start

---
+++ js/productCarousel.js
@@ -0,0 +1,13 @@
+const slider = tns({
+  container: '.product-carousel .slider-list',
+  items: 1,
+  navContainer: '.product-carousel .thumbnail-list',
+  controls: false,
+  navAsThumbnails: true,
+  arrowKeys: true,
+  autoplay: true,
+  autoplayHoverPause: true,
+  autoplayButtonOutput: false,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+})
