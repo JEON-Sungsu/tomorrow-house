63d7cb4c230440088a8f881ac5e6cee26ab069ae
JEON-Sungsu <63297236+JEON-Sungsu@users.noreply.github.com>
2022-02-04 18:25:51 +0900

    뭐 많이 했네 여튼 갤러리 모바일 & 데스크탑 나누기 완료

--- js/Carousel.js
+++ js/Carousel.js
@@ -12,11 +12,11 @@ const slider = tns({
   preventScrollOnTouch: true,
 })
 
-const userGallery = tns({
-  container: '.user-gallery .slider-list',
-  navContainer: '.user-gallery-thumbnail .thumbnail-list',
+const userGalleryMobile = tns({
+  container: '.user-gallery.is-mobile .slider-list',
+  navContainer: '.user-gallery.is-mobile .user-gallery-thumbnail .thumbnail-list',
   controls: false,
-  controlsContainer: '.user-gallery-controls', //컨트롤 이동버튼 커스터마이징 컨테이너 지정
+  controlsContainer: '.user-gallery.is-mobile .user-gallery-controls', //컨트롤 이동버튼 커스터마이징 컨테이너 지정
   gutter: 4, //슬라이드별 간격 px 임
   edgePadding: 16, //다음 슬라이드를 양옆에 보여지게 해주는 간격 px 임
   loop: false, //슬라이드 무한반복 이걸하면 edgePadding 을 주더라도 제일 첫 슬라이드에서 양옆이 아니라 우측으로만 드러난다.
@@ -25,11 +25,25 @@ const userGallery = tns({
   mouseDrag: true,
   preventScrollOnTouch: true,
 
-  responsive: {
-    768: {
-      gutter: 6,
-      controls: true,
-      edgePadding: 75,
-    }, //tablet 반응형
-  },
+  // responsive: {
+  //   768: {
+  //     gutter: 6,
+  //     controls: true,
+  //     edgePadding: 75,
+  //   }, //tablet 반응형
+  // }
+})
+
+const userGallery = tns({
+  container: '.user-gallery.is-desktop .slider-list',
+  navContainer: '.user-gallery.is-desktop .user-gallery-thumbnail .thumbnail-list',
+  controlsContainer: '.user-gallery.is-desktop .user-gallery-controls', //컨트롤 이동버튼 커스터마이징 컨테이너 지정
+  controls: true,
+  gutter: 6, //슬라이드별 간격 px 임
+  edgePadding: 75, //다음 슬라이드를 양옆에 보여지게 해주는 간격 px 임
+  loop: false, //슬라이드 무한반복 이걸하면 edgePadding 을 주더라도 제일 첫 슬라이드에서 양옆이 아니라 우측으로만 드러난다.
+  arrowKeys: true,
+  autoplayHoverPause: true,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
 })
