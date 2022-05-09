110d3818edd590d469a016884404ab0692566b9e
JEON Sungsu <anagma@sjsofttech.co.kr>
2022-05-09 22:12:23 +0900

    개별 검색 히스토리 삭제 기능 추가

--- js/search.js
+++ js/search.js
@@ -4,6 +4,7 @@ const gnbSearchHistory = gnbSearch.querySelector('.search-history')
 const gnbSearchHistoryList = gnbSearchHistory.querySelector('.search-history-list')
 
 const deleteAllButton = gnbSearchHistory.querySelector('.search-history-header button')
+const deleteListButton = gnbSearchHistoryList.querySelectorAll('.search-history-item .delete-button')
 
 function closeDropDown() {
   window.addEventListener('click', function (e) {
@@ -18,7 +19,6 @@ function dropDownOpen() {
   if (gnbSearchHistoryList.children.length == 0) {
     return
   }
-
   if (!gnbSearchHistory.classList.contains('is-active')) {
     window.addEventListener('click', closeDropDown)
   }
@@ -30,4 +30,17 @@ gnbSearchInput.addEventListener('focus', dropDownOpen)
 deleteAllButton.addEventListener('click', function () {
   gnbSearchHistoryList.innerHTML = ''
   gnbSearchHistory.classList.remove('is-active')
+})
+
+function deleteSearchHistoryItem(e) {
+  const itemToDelete = this.parentNode
+  gnbSearchHistoryList.removeChild(itemToDelete)
+  e.stopPropagation()
+  if (gnbSearchHistoryList.children.length == 0) {
+    gnbSearchHistory.remove('is-active')
+  }
+}
+
+deleteListButton.forEach(function (el) {
+  el.addEventListener('click', deleteSearchHistoryItem)
 })
